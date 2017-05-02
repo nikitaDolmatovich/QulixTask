923592b9e48c65b946b96baf23e40a504ec1cfa7
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-05-03 02:00:56 +0300

    Add view get all workers commit

--- Qulix/Qulix.Logic/Controllers/WorkerController.cs
+++ Qulix/Qulix.Logic/Controllers/WorkerController.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Web;
 using System.Configuration;
-using System.Web.Http;
+using System.Web.Mvc;
 using Qulix.Domain.Repository;
 using Qulix.Domain.Models;
+using Qulix.Logic.Models;
 
 namespace Qulix.Logic.Controllers
-{ 
-    [RoutePrefix("Worker")]
-    public class WorkerController : ApiController
+{
+    public class WorkerController : Controller
     {
         private WorkerRepository repo;
 
@@ -19,6 +18,33 @@ namespace Qulix.Logic.Controllers
         {
             string connectionString = ConfigurationManager.ConnectionStrings["WorkerManager"].ConnectionString;
             repo = new WorkerRepository(connectionString);
+        }
+
+        public ActionResult GetAll()
+        {
+            List<WorkerViewModel> workers = new List<WorkerViewModel>();
+
+            foreach(var worker in repo.GetAll())
+            {
+                workers.Add(CopyToViewModel(worker));
+            }
+
+            return View(workers);
+        }
+
+        private WorkerViewModel CopyToViewModel(Worker worker)
+        {
+            return new WorkerViewModel()
+            {
+                WorkerId = worker.WorkerId,
+                Name = worker.Name,
+                Surname = worker.Surname,
+                Patronymic = worker.Patronymic,
+                DateRecruitment = worker.DateRecruitment,
+                Position = worker.Position,
+                CompanyId = worker.CompanyId
+            };
+
         }
     }
 }
