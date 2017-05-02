e7ebbd1a4957a994955a31eca3b748307692b967
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-05-03 00:27:37 +0300

    Add ViewModels commit

---
+++ Qulix/Qulix.Logic/Models/CompanyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qulix.Logic.Models
+{
+    public class CompanyViewModel
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public int SizeCompany { get; set; }
+        public string OrganizationalForm { get; set; }
+    }
+}
---
+++ Qulix/Qulix.Logic/Models/WorkerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Qulix.Logic.Models
+{
+    public class WorkerViewModel
+    {
+        public int WorkerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DateRecruitment { get; set; }
+        public string Position { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
