31771c7ab05fd058374262e03c0f9753e5c9df51
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-05-02 23:59:36 +0300

    Add Get all worker from angular commit

---
+++ Qulix/Qulix.Logic/Angular/Controllers/Controllers.js
@@ -0,0 +1,9 @@
+﻿angular.module("ManagerApp.Controllers", ["ManagerApp.Services"])
+
+    .controller("WorkerController", ["WorkerFactory", "$scope", function (workerFactory, $scope) {
+        $scope.workers = workerFactory.all();
+    }])
+
+    .controller("CompanyController", ["CompanyFactory", "$scope", "$routeParams", function (productFactory, $scope, $routeParams) {
+        $scope.products = productFactory.get({ id: $routeParams.id });
+    }]);
---
+++ Qulix/Qulix.Logic/Angular/Services/Services.js
@@ -0,0 +1,13 @@
+﻿angular.module("ManagerApp.Services", ["ngResource"])
+
+.factory("WorkerFactory", ["$resource", function ($resource) {
+    return $resource("worker", {}, {
+        all: { method: "GET", isArray: true }
+    });
+}])
+
+.factory("CompanyFactory", ["$resource", function ($resource) {
+    return $resource("shop/:id/products", { id: "@id" }, {
+        get: { method: "GET", isArray: true }
+    });
+}]);
