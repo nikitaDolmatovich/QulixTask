2045fc0c4c2c4b5b8fd3e9cdc2cc8098f1c02dd3
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-05-03 00:12:56 +0300

    Add connection string commit

---
+++ Qulix/Qulix.Logic/Controllers/WorkerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Qulix.Logic.Controllers
+{ 
+    [RoutePrefix("Worker")]
+    public class WorkerController : ApiController
+    {
+              
+    }
+}
