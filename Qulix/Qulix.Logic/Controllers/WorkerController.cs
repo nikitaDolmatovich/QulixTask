9bd71a324d69f5e9abdaee6c8f28bf97d813088a
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-05-03 00:25:58 +0300

    Add connection string to WorkerController commit

--- Qulix/Qulix.Logic/Controllers/WorkerController.cs
+++ Qulix/Qulix.Logic/Controllers/WorkerController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Configuration;
 using System.Web.Http;
+using Qulix.Domain.Repository;
+using Qulix.Domain.Models;
 
 namespace Qulix.Logic.Controllers
 { 
     [RoutePrefix("Worker")]
     public class WorkerController : ApiController
     {
-              
+        private WorkerRepository repo;
+
+        public WorkerController()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["WorkerManager"].ConnectionString;
+            repo = new WorkerRepository(connectionString);
+        }
     }
 }
