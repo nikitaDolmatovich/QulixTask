ba301715082b19654a4485b7b5e278b33c3893e5
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-05-02 20:54:25 +0300

    Add models commit

---
+++ Qulix/Qulix.Domain/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qulix.Domain.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public int SizeCompany { get; set; }
+        public string OrganizationalForm { get; set; }
+    }
+}
---
+++ Qulix/Qulix.Domain/Models/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qulix.Domain.Models
+{
+    public class Worker
+    {
+        public int WorkerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DateRecruitment { get; set; }
+        public string Position { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
